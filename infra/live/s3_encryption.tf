abd858cb53e6e79128ed341b3b23d858e346fe15
Leandro de O Barbosa <leandrodeo.barbosa@gmail.com>
2026-03-20 14:17:27 -0300

    refactor(terraform): merged S3-related resources into a single file s3.tf

--- infra/live/s3_encryption.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket_server_side_encryption_configuration" "portfolio_static_site" {
-  bucket = aws_s3_bucket.portfolio_static_site.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
--- infra/live/s3_versioning.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_s3_bucket_versioning" "portfolio_static_site" {
-  bucket = aws_s3_bucket.portfolio_static_site.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
