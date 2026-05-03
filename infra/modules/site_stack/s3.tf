8eed8732f17085981eb73e7b4e54a5806039084b
Leandro de O Barbosa <leandrodeo.barbosa@gmail.com>
2026-05-03 19:32:51 -0300

    fix(terraform): explicitly block S3 SSE-C encryption

--- infra/modules/site_stack/s3.tf
+++ infra/modules/site_stack/s3.tf
@@ -9,7 +9,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "portfolio_static_
 
   rule {
     bucket_key_enabled       = false
-    blocked_encryption_types = ["NONE"]
+    blocked_encryption_types = ["SSE-C"]
 
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
